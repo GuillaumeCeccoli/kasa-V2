a2c1f054318031d569028d36690f18b38be60609
Guillaume Ceccoli <guiceccoli@gmail.com>
2023-06-09 10:34:43 +0200

    synchro

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -9,6 +9,10 @@ export default function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
+        {/* On définit la page par défaut */}
+        <Route index element={<Home />} />
+
+        {/* On définit nos routes ci-dessous */}
         <Route path="/home" element={<Home />} />
         <Route path="/apropos" element={<Apropos />} />
         <Route path="*" element={<Error />} />
