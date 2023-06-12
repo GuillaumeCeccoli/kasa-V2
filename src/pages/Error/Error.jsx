dd25228f578dd3b0ab604dc77964816d0f1953ab
Guillaume Ceccoli <guiceccoli@gmail.com>
2023-06-12 10:37:07 +0200

    Layout error 404

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import "./error.css";
+
 export default function Error() {
   return (
-    <div>
-      <h1>Ceci est la page d'erreur !</h1>
+    <div className="errorContain">
+      <h1 className="errorNumber">404</h1>
+      <p className="errorText">
+        Oups! La page que <span>vous demandez n'existe pas.</span>
+      </p>
+      <Link className="returnHome" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 }
